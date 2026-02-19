563f028c5eb50617ba4d5f2f0e19e9d9e644522a
Neeraj Singh <hellocloud.webdev@gmail.com>
2026-02-20 00:16:40 +0530

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { NewsComponent } from "@/components/news/news-component";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-background">
-      <NewsComponent />
-    </div>
-  );
-}
